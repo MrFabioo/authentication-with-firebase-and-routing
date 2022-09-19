f74324c39e8a80db3b3ce237b86ffb06e70e65d1
Kamil Słomiński <kam_slomka@wp.pl>
2022-09-19 02:56:43 +0200

    Login Component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { Card, Button, Form, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 export default function Signup() {
@@ -53,7 +54,7 @@ export default function Signup() {
         </Card.Body>
       </Card>
       <div className='w-100 text-center mt-2'>
-        Already have an account? Log in
+        Already have an account? <Link to='/login'>Log in</Link>
       </div>
     </>
   );
