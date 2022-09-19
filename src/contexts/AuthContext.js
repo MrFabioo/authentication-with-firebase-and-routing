a0155ecf9ae366c00a04a34059ebe77153cc220f
Kamil Słomiński <kam_slomka@wp.pl>
2022-09-19 02:10:15 +0200

    Auth context setup -bug fix

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,31 +1,30 @@
-import React, { useContext, useState,useEffect } from 'react';
-import {auth} from '../firebase'
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase';
 
 const AuthContext = React.createContext();
 
-export funciton useAuth() {
-  return useContext(AuthContext)
+export function useAuth() {
+  return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }) {
-  const [currentUser,setCurrentUser] = useState()
+  const [currentUser, setCurrentUser] = useState();
 
-  function signup(email,password){
-    return auth.createUserWithEmailAndPassword(email,password)
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
   }
 
-  useEffect(()=>{
-    const unsubscribe = auth.onAuthStateChanged(user=>{
-      setCurrentUser(user)
-    })
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
 
-    return unsubscribe
-  },[])
+    return unsubscribe;
+  }, []);
 
-
-  const value ={
+  const value = {
     currentUser,
-    signup
-  }
+    signup,
+  };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
