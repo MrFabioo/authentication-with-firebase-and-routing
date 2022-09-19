0cf6881a3214cf2601916cf79ebaf01486b00a01
Kamil Słomiński <kam_slomka@wp.pl>
2022-09-19 03:17:41 +0200

    Login and Signup to Dashboard useNavigate

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,19 @@
 import React, { useRef, useState } from 'react';
 import { Card, Button, Form, Alert } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const { signup } = useAuth();
+  const { login } = useAuth();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/');
+  }
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -16,9 +21,10 @@ export default function Login() {
     try {
       setError('');
       setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
+      await login(emailRef.current.value, passwordRef.current.value);
+      handleClick();
     } catch {
-      setError('Failed to create an account');
+      setError('Failed to log in');
     }
     setLoading(false);
   }
